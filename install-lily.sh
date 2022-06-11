f1fa2255304e551d394148edc006f21491595548
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2022-06-11 14:06:12 +0200

    Add bzip2 to installation requirements

--- install-lily.sh
+++ install-lily.sh
@@ -3,7 +3,8 @@
 apt --no-install-recommends -qq -y install \
 ca-certificates \
 curl \
-unzip
+unzip \
+bzip2
 
 curl -LO https://lilypond.org/download/binaries/linux-64/lilypond-2.22.2-1.linux-64.sh; \
 sh lilypond-2.22.2-1.linux-64.sh --batch; \
