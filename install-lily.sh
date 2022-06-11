75d08afa002edc7ca38a94ecb89fb8da59cbf9fb
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2022-06-11 13:02:49 +0200

    Update Lilypond

--- install-lily.sh
+++ install-lily.sh
@@ -5,8 +5,8 @@ ca-certificates \
 curl \
 unzip
 
-curl -LO https://lilypond.org/download/binaries/linux-64/lilypond-2.20.0-1.linux-64.sh; \
-sh lilypond-2.20.0-1.linux-64.sh --batch; \
-rm lilypond-2.20.0-1.linux-64.sh
+curl -LO https://lilypond.org/download/binaries/linux-64/lilypond-2.22.2-1.linux-64.sh; \
+sh lilypond-2.22.2-1.linux-64.sh --batch; \
+rm lilypond-2.22.2-1.linux-64.sh
 
 apt -qq purge -y curl unzip ca-certificates && apt -qq autoremove -y --purge && apt -qq clean
