45f76590c23bb54b400a9dd5b3ad1b7de4131daf
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2023-06-07 23:52:13 +0200

    Fix lilypond installation

--- install-lily.sh
+++ install-lily.sh
@@ -3,10 +3,11 @@
 apt --no-install-recommends -qq -y install \
 ca-certificates \
 curl \
-unzip
+unzip \
+bzip2
 
 curl -LO https://lilypond.org/download/binaries/linux-64/lilypond-2.22.2-1.linux-64.sh; \
 sh lilypond-2.22.2-1.linux-64.sh --batch; \
 rm lilypond-2.22.2-1.linux-64.sh
 
-apt -qq purge -y curl unzip ca-certificates && apt -qq autoremove -y --purge && apt -qq clean
+apt -qq purge -y curl unzip bzip2 ca-certificates && apt -qq autoremove -y --purge && apt -qq clean
