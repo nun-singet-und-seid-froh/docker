ef573eea36381a3d576e50f5ec76148ee657be8d
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2023-06-07 22:01:49 +0200

    Update lilypond and use ghcr

--- install-lily.sh
+++ install-lily.sh
@@ -6,8 +6,8 @@ curl \
 unzip \
 bzip2
 
-curl -LO https://lilypond.org/download/binaries/linux-64/lilypond-2.24.1.linux-64.sh; \
-sh lilypond-2.24.1.linux-64.sh --batch; \
-rm lilypond-2.24.1.linux-64.sh
+curl -LO https://gitlab.com/lilypond/lilypond/-/releases/v2.24.1/downloads/lilypond-2.24.1-linux-x86_64.tar.gz; \
+tar -xf lilypond-2.24.1-linux-x86_64.tar.gz --strip-components=1 -C /usr/local; \
+rm lilypond-2.24.1-linux-x86_64.tar.gz
 
 apt -qq purge -y curl unzip ca-certificates && apt -qq autoremove -y --purge && apt -qq clean
