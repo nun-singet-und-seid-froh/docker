dcf47044242edf620fbf18abb68ad9d72b5c205d
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2024-10-23 22:41:23 +0200

    Use ubuntu lilypond

--- install-lily.sh
+++ install-lily.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-apt --no-install-recommends -qq -y install \
-ca-certificates \
-curl \
-unzip \
-bzip2
-
-curl -LO https://lilypond.org/download/binaries/linux-64/lilypond-2.22.2-1.linux-64.sh; \
-sh lilypond-2.22.2-1.linux-64.sh --batch; \
-rm lilypond-2.22.2-1.linux-64.sh
-
-apt -qq purge -y curl unzip bzip2 ca-certificates && apt -qq autoremove -y --purge && apt -qq clean
+apt --no-install-recommends -qq -y install lilypond
+apt -qq autoremove -y --purge && apt -qq clean
